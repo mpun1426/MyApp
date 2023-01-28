023cff3463ff9bd1a0eb0eac2bdae90cab6fae9c
mpun1426 <mpun1426@gmail.com>
2023-01-29 02:35:31 +0900

    [add]ゲストログイン機能追加
    採用企業様やゲスト様向けにゲストログイン機能を実装しました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,13 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   mount_uploader :avatar, AvatarUploader
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  def self.guest
+    find_or_create_by!(email: 'guest@email.com') do |user|
+      user.nickname = "ゲストユーザー"
+      user.introduction = "ゲストユーザーです。"
+      user.password = SecureRandom.urlsafe_base64
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'shareco#index'
+  devise_for :users
+  devise_scope :user do
+    post 'users/guest_login', to: 'users/sessions#guest_login'
+  end
+  get 'users/account'
   resources :shareco
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
