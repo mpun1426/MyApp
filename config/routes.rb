6e406b691b0cedc4b29cba36c354f8a42fd2b1e9
mpun1426 <mpun1426@gmail.com>
2023-02-06 04:47:34 +0900

    [change]ゲストユーザーの編集・削除を不可とする実装
    ゲストユーザーを編集および削除できないように制御しました。

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'shareco#index'
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   devise_scope :user do
     post 'users/guest_login', to: 'users/sessions#guest_login'
     get 'users', to: redirect('users/sign_up')
