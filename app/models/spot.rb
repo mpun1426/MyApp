3b0084bd162b43d42a388520d08c659d23c7563a
mpun1426 <mpun1426@gmail.com>
2023-02-13 01:24:34 +0900

    [update]画像投稿機能アップデート
    バリデーション文言の変更、ビューの画像サイズ等を整えました。

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -5,6 +5,6 @@ class Spot < ApplicationRecord
   validates :name, length: {in: 1..25}
   validates :address, length: {in: 1..30}
   validates :images, content_type: { in: ['image/jpeg', 'image/jpg', 'image/gif', 'image/png'],
-            message: "有効なフォーマットではありません" },
-            size: { less_than: 5.megabytes, message: "5MBを超える画像はアップロードできません" }
+            message: "対応ファイルは jpeg、jpg、gif、png です" },
+            size: { less_than: 5.megabytes, message: "アップロードできるファイルサイズは5MBまでです" }
 end
