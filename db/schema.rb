7625936b148fe939054ce43c9744ec30a76225a6
mpun1426 <mpun1426@gmail.com>
2023-02-16 20:36:11 +0900

    [remove]usersテーブルavatarカラム削除
    ユーザーアバターをCarrierwaveからActiveStorageでの実装に変更の為、不要となったavatarカラムを削除しました。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_11_190041) do
+ActiveRecord::Schema.define(version: 2023_02_16_104519) do
 
   create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
@@ -56,7 +56,6 @@ ActiveRecord::Schema.define(version: 2023_02_11_190041) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "introduction"
-    t.string "avatar"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
