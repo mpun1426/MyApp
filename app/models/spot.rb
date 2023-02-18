6d959540489d73b930870a8a0a71be19617ca266
mpun1426 <mpun1426@gmail.com>
2023-02-19 03:20:35 +0900

    [change]Spotバリデーション編集
    定数を用いたバリデーションとすることで保守性を向上させました。

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,10 +1,13 @@
 class Spot < ApplicationRecord
   belongs_to :user
   has_many_attached :images
+  MAX_IMAGES_COUNT = 4
+  MAX_IMAGE_SIZE = 2
 
-  validates :name, length: {in: 1..25}
-  validates :address, length: {in: 1..30}
+  validates :name, length: { in: 1..25 }
+  validates :address, length: { in: 1..30 }
   validates :images, content_type: { in: ['image/jpeg', 'image/jpg', 'image/gif', 'image/png'],
             message: "対応ファイルは jpeg、jpg、gif、png です" },
-            size: { less_than: 5.megabytes, message: "アップロードできるファイルサイズは5MBまでです" }
+            size: { less_than: MAX_IMAGE_SIZE.megabytes, message: "アップロードできるファイルサイズは#{ MAX_IMAGE_SIZE }MBまでです" },
+            limit: { max: MAX_IMAGES_COUNT }
 end
