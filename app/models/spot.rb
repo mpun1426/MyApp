55169dd3707a04cc1943b2b9a8b9c4d8bd42d0f6
mpun1426 <mpun1426@gmail.com>
2023-02-25 23:44:04 +0900

    [update]Spotバリデーションのアップデート
    バリデーションについて、レンダリング時のレイアウト崩れ等一部修正し、全体的にアップデートしました。

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -6,8 +6,7 @@ class Spot < ApplicationRecord
 
   validates :name, length: { in: 1..25 }
   validates :address, length: { in: 1..30 }
-  validates :images, content_type: { in: ['image/jpeg', 'image/jpg', 'image/gif', 'image/png'],
-            message: "対応ファイルは jpeg、jpg、gif、png です" },
-            size: { less_than: MAX_IMAGE_SIZE.megabytes, message: "アップロードできるファイルサイズは#{ MAX_IMAGE_SIZE }MBまでです" },
-            limit: { max: MAX_IMAGES_COUNT }
+  validates :images, content_type: { in: ['image/jpeg', 'image/jpg', 'image/gif', 'image/png'], message: "の対応ファイルは jpeg、jpg、gif、png です" }
+  validates :images, size: { less_than: MAX_IMAGE_SIZE.megabytes, message: "ファイルのサイズは1枚につき#{ MAX_IMAGE_SIZE }MBまでです" }
+  validates :images, limit: { max: MAX_IMAGES_COUNT, message: "をアップロードできるのは#{ MAX_IMAGES_COUNT }枚までです" }
 end
