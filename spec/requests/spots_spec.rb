86e23139951ee087ef9a3fda9f17185707fec920
mpun1426 <mpun1426@gmail.com>
2023-02-25 23:56:02 +0900

    [refactor]rubocop解析にてリファクタリング
    rubocop_airbnbでの解析にてコードをリファクタリングしました。

--- spec/requests/spots_spec.rb
+++ spec/requests/spots_spec.rb
@@ -13,17 +13,11 @@ require 'rails_helper'
 # sticking to rails and rspec-rails APIs to keep things simple and stable.
 
 RSpec.describe "/spots", type: :request do
-  
   # This should return the minimal set of attributes required to create a valid
   # Spot. As you add validations to Spot, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
-  }
-
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
-  }
+  let(:valid_attributes) { skip("Add a hash of attributes valid for your model") }
+  let(:invalid_attributes) { skip("Add a hash of attributes invalid for your model") }
 
   describe "GET /index" do
     it "renders a successful response" do
@@ -59,9 +53,7 @@ RSpec.describe "/spots", type: :request do
   describe "POST /create" do
     context "with valid parameters" do
       it "creates a new Spot" do
-        expect {
-          post spots_url, params: { spot: valid_attributes }
-        }.to change(Spot, :count).by(1)
+        expect { post spots_url, params: { spot: valid_attributes } }.to change(Spot, :count).by(1)
       end
 
       it "redirects to the created spot" do
@@ -72,25 +64,19 @@ RSpec.describe "/spots", type: :request do
 
     context "with invalid parameters" do
       it "does not create a new Spot" do
-        expect {
-          post spots_url, params: { spot: invalid_attributes }
-        }.to change(Spot, :count).by(0)
+        expect { post spots_url, params: { spot: invalid_attributes } }.to change(Spot, :count).by(0)
       end
 
-    
       it "renders a successful response (i.e. to display the 'new' template)" do
         post spots_url, params: { spot: invalid_attributes }
         expect(response).to be_successful
       end
-    
     end
   end
 
   describe "PATCH /update" do
     context "with valid parameters" do
-      let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
-      }
+      let(:new_attributes) { skip("Add a hash of attributes valid for your model") }
 
       it "updates the requested spot" do
         spot = Spot.create! valid_attributes
@@ -108,22 +94,18 @@ RSpec.describe "/spots", type: :request do
     end
 
     context "with invalid parameters" do
-    
       it "renders a successful response (i.e. to display the 'edit' template)" do
         spot = Spot.create! valid_attributes
         patch spot_url(spot), params: { spot: invalid_attributes }
         expect(response).to be_successful
       end
-    
     end
   end
 
   describe "DELETE /destroy" do
     it "destroys the requested spot" do
       spot = Spot.create! valid_attributes
-      expect {
-        delete spot_url(spot)
-      }.to change(Spot, :count).by(-1)
+      expect { delete spot_url(spot) }.to change(Spot, :count).by(-1)
     end
 
     it "redirects to the spots list" do
