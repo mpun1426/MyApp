fdaf0e208bc21bf6f520dede35e14a3aefc81716
mpun1426 <mpun1426@gmail.com>
2023-03-19 15:15:18 +0900

    [update]いいね機能のAjax対応、実装完了
    いいね機能をAjax対応させ、いいね機能の実装完了としました。

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,15 +1,13 @@
 class LikesController < ApplicationController
   def create
+    @spot = Spot.find(params[:spot_id])
     like = current_user.likes.new(spot_id: params[:spot_id])
     like.save
-    flash[:notice] = "いいねしました"
-    redirect_back(fallback_location: root_path)
   end
 
   def destroy
+    @spot = Spot.find(params[:spot_id])
     like = current_user.likes.find_by(spot_id: params[:spot_id])
     like.destroy
-    flash[:notice] = "いいねを削除しました"
-    redirect_back(fallback_location: root_path)
   end
 end
