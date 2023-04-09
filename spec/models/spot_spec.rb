504e343f19d3b313753e908d625ba8156168966a
mpun1426 <mpun1426@gmail.com>
2023-04-10 02:15:11 +0900

    [test]Spotモデルスペック実施完了
    Spotモデルスペックのテストが完了しました。

--- spec/models/spot_spec.rb
+++ spec/models/spot_spec.rb
@@ -1,5 +1,72 @@
 require 'rails_helper'
 
 RSpec.describe Spot, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "spots" do
+    let(:spot) { create(:spot) }
+
+    context "有効な値でスポットを投稿する場合" do
+      it "spotが有効であること" do
+        expect(spot).to be_valid
+      end
+
+      it "スポット名が1文字以上の場合、spotが有効であること" do
+        spot.name = "n"
+        expect(spot).to be_valid
+      end
+
+      it "スポット名が25文字以下の場合、spotが有効であること" do
+        spot.name = "n" * 25
+        expect(spot).to be_valid
+      end
+
+      it "所在地が1文字以上の場合、spotが有効であること" do
+        spot.address = "n"
+        expect(spot).to be_valid
+      end
+
+      it "所在地が30文字以下の場合、spotが有効であること" do
+        spot.address = "n" * 30
+        expect(spot).to be_valid
+      end
+
+      it "imageのサイズが2MB以内の場合、spotが有効であること" do
+        spot.images = [Rack::Test::UploadedFile.new('spec/fixtures/images/1MB.jpeg', 'image/jpeg')]
+        expect(spot).to be_valid
+      end
+    end
+
+    context "無効な値でスポットを投稿する場合" do
+      it "スポット名が1文字未満の場合、spotが無効であること" do
+        spot.name = ""
+        expect(spot).not_to be_valid
+      end
+
+      it "スポット名が25文字を超える場合、spotが無効であること" do
+        spot.name = "n" * 26
+        expect(spot).not_to be_valid
+      end
+
+      it "所在地が1文字未満の場合、spotが無効であること" do
+        spot.address = ""
+        expect(spot).not_to be_valid
+      end
+
+      it "所在地が30文字を超える場合、spotが無効であること" do
+        spot.address = "n" * 31
+        expect(spot).not_to be_valid
+      end
+
+      it "画像のファイル形式が jpeg、jpg、gif、png 以外の場合、spotが無効であること" do
+        spot.images = [Rack::Test::UploadedFile.new('spec/fixtures/other_files/sample.txt', 'text/plain')]
+        expect(spot).not_to be_valid
+        expect(spot.errors[:images]).to include "の対応ファイルは jpeg、jpg、gif、png です"
+      end
+
+      it "画像のサイズが2MBを超える場合、spotが無効であること" do
+        spot.images = [Rack::Test::UploadedFile.new('spec/fixtures/images/3MB.jpeg', 'image/jpeg')]
+        expect(spot).not_to be_valid
+        expect(spot.errors[:images]).to include "ファイルのサイズは1枚につき2MBまでです"
+      end
+    end
+  end
 end
