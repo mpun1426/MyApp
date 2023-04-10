b000fd89021c93db82d649db17a3eac1dd707710
mpun1426 <mpun1426@gmail.com>
2023-04-10 22:36:39 +0900

    [test]Like(いいね！機能)のモデルスペック及びリクエストスペック実施
    Likeのモデルスペック及びリクエストスペックのテストが完了しました。

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+  let(:spot) { create(:spot) }
+  let!(:like) { create(:like, user_id: user.id, spot_id: spot.id) }
+
+  context "有効な値でいいね！する場合" do
+    it "いいね！が有効であること" do
+      expect(like).to be_valid
+    end
+  end
+
+  context "無効な値でいいね！する場合" do
+    it "user_idが空の場合、likeが無効であること" do
+      like.user_id = ""
+      expect(like).not_to be_valid
+    end
+
+    it "spot_idが空の場合、likeが無効であること" do
+      like.spot_id = ""
+      expect(like).not_to be_valid
+    end
+  end
+
+  context 'ユーザーを削除した場合' do
+    it 'ユーザーのいいね！も併せて削除されること' do
+      expect { user.destroy }.to change(Like, :count).by(-1)
+    end
+  end
 end
--- spec/requests/likes_spec.rb
+++ spec/requests/likes_spec.rb
@@ -1,7 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe "Likes", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+  let(:spot) { create(:spot) }
+
+  before do
+    sign_in user
+  end
+
+  describe "create Ajaxでいいね！する" do
+    it "Ajaxでいいね！できること" do
+      expect do
+        post spot_likes_path(spot), params: { like: { user_id: user.id, spot_id: spot.id } }, xhr: true
+      end.
+        to change(Like, :count).by(1)
+    end
+  end
+
+  describe "destroy Ajaxでいいね！解除" do
+    before do
+      post spot_likes_path(spot), params: { like: { user_id: user.id, spot_id: spot.id } }, xhr: true
+    end
+
+    it "Ajaxでいいね！を解除できること" do
+      expect { delete spot_likes_path(spot), xhr: true }.to change(Like, :count).by(-1)
+    end
   end
 end
