b5501346606d2c1e1372898d0ceccbf5bd25e7f3
mpun1426 <mpun1426@gmail.com>
2023-04-11 19:18:23 +0900

    [test/refactor]Spotリクエストスペック 微細なリファクタリング

--- spec/requests/spots_spec.rb
+++ spec/requests/spots_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "/spots", type: :request do
+RSpec.describe "Spots", type: :request do
   let(:user) { create(:user) }
   let(:spot) { create(:spot) }
   let!(:current_user_created_spot) { create(:spot, user_id: user.id) }
